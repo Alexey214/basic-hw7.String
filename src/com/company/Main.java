17f87829e9b4b7be0e2659964e87e014beef7dff
brain <golenev214@gmail.com>
2022-03-21 19:27:54 +0300

    DZ7

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,21 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        //Задание №1:
+        String firstName = "Ivanov";
+        String middleName = "Ivan";
+        String lastName = "Ivanowich";
+        String fullName = firstName + " " + middleName + " " + lastName;
+        System.out.println(fullName);
+
+        //Задание №2:
+        String upperCase = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета - " + upperCase);
+
+        //Задание №3:
+        fullName = "Иванов Семён Семёнович";
+        String change = fullName.replace('ё', 'е');
+        System.out.println(change);
     }
 }
